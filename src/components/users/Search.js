79bc490c2148de6135c385895f4da7534d37e7d2
Sidney Alexandre Pereira Martins <smartins@daitan.com>
2020-07-27 19:59:19 -0300

    Inserting clear button

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -15,6 +15,8 @@ export class Search extends Component {
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
   render() {
+    const { showClear, clearUsers } = this.props;
+
     return (
       <div>
         <form onSubmit={this.onSubmit} className='form'>
@@ -31,6 +33,11 @@ export class Search extends Component {
             className='btn btn-dark btn-block'
           />
         </form>
+        {showClear && (
+          <button className='btn btn-light btn-block' onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
@@ -38,6 +45,8 @@ export class Search extends Component {
 
 Search.propTypes = {
   searchUsers: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
 };
 
 export default Search;
